edd906f087988b9d6374971b6d8b0c6f53e9db07
GamaRayL <gamaizingg@gmail.com>
2022-12-06 14:08:29 +0300

    refactor: some new to modal component and responsive style for media screen

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,33 +1,81 @@
+import { useState } from "react";
+import { Modal } from "components";
 import css from "./styles.module.css";
 
 export const Card = (props) => {
   const { cardId, setCardId, onClick, id, name, category, img } = props;
 
-  const onSubmitCardID = (id) => {
+  const [modalActive, setIsModalActive] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+
+  const onSubmitInclude = (id) => {
     cardId.includes(id)
       ? setCardId([...cardId.filter((item) => item !== id)])
       : setCardId([...cardId, id]);
   };
 
-  return (
-    <div
-      onClick={() => onSubmitCardID(id)}
-      className={`${css.card} ${cardId.includes(id) ? css.bordered : ""}`}
-    >
-      <img className={css.img} src={`./images/${img}`} alt="card" />
-      <div className={css.container}>
-        <p id="log"></p>
-        <div
-          className={css.category}
-          onClick={(e) => {
-            e.stopPropagation();
-            onClick(category);
-          }}
-        >
-          {category}
+  const CardPopup = (
+    <Modal active={modalActive} setActive={setIsModalActive}>
+      <img className={css.modalImage} src={`./images/${img}`} alt="card" />
+      <div className={css.modalCard}>
+        <div>
+          <h2 className={css.modalTitle}>{name}</h2>
+          <p className={css.modalContent}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quae
+            officia officiis nemo ex aut autem, harum error reiciendis rem
+            magnam deserunt ut incidunt illo magni necessitatibus! In error
+            commodi, voluptate sit id, laudantium omnis molestias eius quae unde
+            nostrum at. Quos distinctio quae voluptatum sint rem. Expedita,
+            veritatis reprehenderit?
+          </p>
         </div>
-        <div className={css.name}>{name}</div>
+        <div
+          className={css.modalDeleteIcon}
+          onClick={() => setIsModalActive(false)}
+        >
+          <img src="./images/close.svg" alt="delete icon" />
+        </div>
       </div>
-    </div>
+    </Modal>
+  );
+
+  return (
+    <>
+      {CardPopup}
+      <div
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        onClick={() => setIsModalActive(true)}
+        className={`${css.card}`}
+      >
+        <div onClick={(e) => e.stopPropagation()}>
+          <input
+            className={css.checkmark}
+            id={id}
+            type="checkbox"
+            onChange={() => onSubmitInclude(id)}
+          />
+          <label
+            htmlFor={id}
+            className={`${css.bookmark} ${
+              (isHover || cardId.includes(id)) && css.bookmarkActive
+            }`}
+          ></label>
+        </div>
+        <img className={css.img} src={`./images/${img}`} alt="card" />
+        <div className={css.container}>
+          <div
+            className={css.category}
+            onClick={(e) => {
+              e.stopPropagation();
+              onClick(category);
+            }}
+          >
+            {category}
+          </div>
+          <div className={css.title}>{name}</div>
+        </div>
+      </div>
+    </>
   );
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,3 +1,17 @@
-export const Modal = () => {
-  return <div>Modal</div>;
+import css from "./styles.module.css";
+
+export const Modal = ({ active, setActive, children }) => {
+  return (
+    <div
+      className={`${css.modal} ${active && css.modalActive}`}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={`${css.content} ${active && css.contentActive}`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
 };
